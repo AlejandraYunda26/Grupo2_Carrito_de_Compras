708872697abcf9d37acbaf77277b6585bc0f48e5
AlejandraYunda26 <168245467+AlejandraYunda26@users.noreply.github.com>
2024-04-30 10:07:06 -0500

    Add files via upload

---
+++ cerrar_sesion.php
@@ -0,0 +1,6 @@
+<?php
+// Cerrar la sesión (simulado)
+// Aquí deberías implementar la lógica para cerrar la sesión del usuario
+
+// Redirigir a la página de inicio de sesión
+header("Location: iniciar_sesion.html");
